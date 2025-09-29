9d576e66c6065a3b9fed436000d55e0b2b6c87c5
phet <chvlpf@gmail.com>
2025-09-29 18:02:12 +0700

    Fix taggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Label } from "@/components/ui/label.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { useEffect, useState } from "react";
 import { DialogSaveTodo, todoDefault } from "@/components/save-dialog.tsx";
-import { IconEdit, IconPlus, IconTrashFilled, IconToggleRightFilled } from "@tabler/icons-react";
+import { IconEdit, IconPlus, IconTrashFilled } from "@tabler/icons-react";
 import { DeleteDialog } from "@/components/delete-dialog.tsx";
 import { ConfirmDialog } from "@/components/confirm-dialog.tsx";
 import LoadingScreen from "@/components/loading-screen.tsx";
